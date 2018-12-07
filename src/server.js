b4ec7fe262eddf29daca3c6b33da6e34e5959e38
RowenNaidoo <rowen.naidoo@gmail.com>
2018-12-08 00:23:37 +1100

    change static port for heroku

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //get port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //get instance of express router
 const router = express.Router();
